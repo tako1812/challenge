52c2521a3c92376695b2c46d452ca45aec4c1a84
Tamar Robakidze <116001291+tako1812@users.noreply.github.com>
2024-12-23 22:14:41 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+const takop = document.querySelector('.tako');
+
+const cars =document.querySelector('.carTypes-container');
+const vehicle = document.querySelector(".vehicle5");
+const carDetainsContainer = document.querySelector('.car-details');
+returnedData = [];
+
+const renderViewMorePets = async function () {
+ 
+  const res = await fetch("../json.data");
+  const datas = await res.json();
+  console.log(datas);
+  returnedData.push(datas);
+    
+};
+renderViewMorePets();
+
+console.log(returnedData);
+
+cars.addEventListener("click", function (e) {
+  document.querySelectorAll('[type="checkbox"]').forEach((item) => {
+    
+    returnedData[0].map(function(data) {
+      if(data.category === item.dataset.inputData) {
+        console.log(111110);
+        const html= `
+          <img src="data.photos[0]" class="main-img">
+          <div class="images-row">
+            <img src="data.photos[1]"/>
+            <img src="data.photos[2]">
+            <img src="data.photos[3]">
+          </div>
+        `;
+        carDetainsContainer.innerHTML += html;
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*cars.addEventListener("click", function (e) {
+    document.querySelectorAll('[type="checkbox"]').forEach((item) => {
+     
+      datas.map(function(data) {
+        if(data.category === item.dataset.inputData) {
+          const html= `
+        <img src="" class="main-img">
+            <div class="images-row">
+              <img src=""/>
+              <img src="">
+              <img src="">
+            </div>
+          `;
+        }
+      });
+
+    });
+  });*/
